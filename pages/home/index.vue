3cc4dbad245be3e85905347f9ed02686ee8d3eb0
hairong.pu.o <hairong.pu.o@nio.com>
2021-07-28 13:31:53 +0800

    30

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -13,13 +13,43 @@
           <div class="feed-toggle">
             <ul class="nav nav-pills outline-active">
               <li class="nav-item" v-if="user">
-                <a class="nav-link disabled" href="">Your Feed</a>
+                <nuxt-link class="nav-link"
+                  :class="{active: tab === 'your_feed'}"
+                  exact
+                  :to="{
+                    name: 'home',
+                    query: {
+                      tab: 'your_feed'
+                    }
+                  }">
+                  Your Feed
+                </nuxt-link>
               </li>
               <li class="nav-item">
-                <a class="nav-link active" href="">Global Feed</a>
+                <nuxt-link class="nav-link"
+                  :class="{active: tab === 'global_feed'}"
+                  exact
+                  :to="{
+                    name: 'home',
+                    query: {
+                      tab: 'global_feed'
+                    }
+                  }">
+                  Global Feed
+                </nuxt-link>
               </li>
               <li class="nav-item" v-if="tag">
-                <a class="nav-link active" href="">#{{tag}}</a>
+                <nuxt-link class="nav-link"
+                  :class="{active: tab === 'tag'}"
+                  :to="{
+                    name: 'home',
+                    query: {
+                      tab: 'tag',
+                      tag
+                    }
+                  }">
+                  #{{tag}}
+                </nuxt-link>
               </li>
             </ul>
           </div>
@@ -102,7 +132,8 @@
                 :to="{
                   name: 'home',
                   query: {
-                    tag
+                    tab: 'tag',
+                    tag,
                   }
                 }" 
                 class="tag-pill tag-default"
@@ -143,17 +174,18 @@ export default {
 
     const { articles, articlesCount } = articleRes.data
     const { tags} = tagRes.data
-
+    
     return {
       articles,
       articlesCount,
       tags,
       limit,
       page,
-      tag
+      tag,
+      tab: query.tab || 'global_feed'
     }
   },
-  watchQuery: ['page', 'tag'],
+  watchQuery: ['page', 'tag', 'tab'],
   computed: {
     ...mapState(['user']),
     totalPage () {
